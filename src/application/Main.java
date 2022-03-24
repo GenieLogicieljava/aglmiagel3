d98584f64b6dd5015c5a1af856872df2431de822
Devgrace001 <gracecompteetudiant@gmail.com>
2022-03-24 04:43:43 +0000

    commit: ajout Logo du stage de l'application et Modification du main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,35 @@
-
 package application;
 	
-import java.io.IOException;
-import java.net.URL;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
-
-public final class Main extends Application {
-
-	  @Override
-	  public void start(final Stage primaryStage) {
-	    try {
-	      final URL url = getClass().getResource("Authentification.fxml");
-	      final FXMLLoader fxmlLoader = new FXMLLoader(url);
-	      // final AnchorPane root = (AnchorPane) fxmlLoader.load();
-	      final BorderPane root = (BorderPane) fxmlLoader.load();
-	      final Scene scene = new Scene(root, 700, 500);
-	      primaryStage.setScene(scene);
-	    } catch (IOException ex) {
-	      System.err.println("Erreur au chargement: " + ex);
-	    }
-	    primaryStage.setTitle("Test FXML");
-	    primaryStage.show();
-	  }
-
-	  public static void main(String[] args) {
-	    launch(args);
-	  }
+public class Main extends Application {
+	//private static Scene scene;
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			
+			Parent root = FXMLLoader.load(getClass().getResource("Authentification.fxml"));
+			
+			primaryStage.setTitle("Logiciel De Gestion Restaurant~chap.chap by groupe 9 ");
+			primaryStage.setScene(new Scene (root));
+			
+			primaryStage.getIcons().add(
+					new Image(
+							Main.class.getResourceAsStream("Iconeexe.jpg")));
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
